a98a361c7970111c73d3abbdf7cf218765a1cbb5
Suprovho <106928567+Suprovho@users.noreply.github.com>
2024-12-21 18:54:59 +0530

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -141,7 +141,7 @@ export const renderParagraphWithLinks = (apiResponse) => {
 };
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
+  "https://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
 
 export const LIVE_CHAT_COUNT = 10;
 
