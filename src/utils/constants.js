d0e30116b9bcea3a407712a831668ab4e755b883
Suprovho <106928567+Suprovho@users.noreply.github.com>
2024-12-21 13:01:51 +0530

    ..

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
-
 export const YOUTUBE_VIDEOS_API =
-"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50"
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50";
 
 export const formatViewCount = (viewCount) => {
   if (viewCount >= 1000000) {
@@ -9,42 +8,93 @@ export const formatViewCount = (viewCount) => {
     return (viewCount / 1000).toFixed(1) + "K";
   } else {
     return `${viewCount}`;
-
   }
 };
 
 // List of category IDs
 const popularRegionCodes = [
-  "US", "IN", "GB", "CA", "AU", "BR", "FR", "DE", "KR", "MX",
-  "JP", "IT", "RU", "ES", "ZA", "NG", "PL", "AR", "PH", "ID",
-  "EG", "TH", "PK", "IT", "VN", "SA", "TR", "CL", "CO", "UA",
-  "MY", "PE", "NG", "SG", "KR", "NG", "ZA", "UA", "PK", "TW",
-    "KH", "DZ", "RO", "PT", "TZ", "AE", "HN", "KE", "LB", "LK",
-    "KR", "LY", "GH", "HN", "TZ", "BG", "DO", "PR", "MT"
-  ];
-  
-  export const getRandomRegionId = () => {
-    const randomIndex = Math.floor(Math.random()*popularRegionCodes.length);
-    return popularRegionCodes[randomIndex];  
-  };
-  
+  "US",
+  "IN",
+  "GB",
+  "CA",
+  "AU",
+  "BR",
+  "FR",
+  "DE",
+  "KR",
+  "MX",
+  "JP",
+  "IT",
+  "RU",
+  "ES",
+  "ZA",
+  "NG",
+  "PL",
+  "AR",
+  "PH",
+  "ID",
+  "EG",
+  "TH",
+  "PK",
+  "IT",
+  "VN",
+  "SA",
+  "TR",
+  "CL",
+  "CO",
+  "UA",
+  "MY",
+  "PE",
+  "NG",
+  "SG",
+  "KR",
+  "NG",
+  "ZA",
+  "UA",
+  "PK",
+  "TW",
+  "KH",
+  "DZ",
+  "RO",
+  "PT",
+  "TZ",
+  "AE",
+  "HN",
+  "KE",
+  "LB",
+  "LK",
+  "KR",
+  "LY",
+  "GH",
+  "HN",
+  "TZ",
+  "BG",
+  "DO",
+  "PR",
+  "MT",
+];
+
+export const getRandomRegionId = () => {
+  const randomIndex = Math.floor(Math.random() * popularRegionCodes.length);
+  return popularRegionCodes[randomIndex];
+};
+
 export function convertToNumber(number) {
   return Number(number).toLocaleString();
 }
 
 export function currentDateTime(props) {
-  
   let dateString = props;
-  
+
   // Convert the string to a JavaScript Date object
   let date = new Date(dateString);
-  
+
   // Get the current date
   let currentDate = new Date();
-  
+
   // Calculate the time difference in milliseconds
   let timeDifference = currentDate - date;
-  
+
   // Format the date
   let formattedDate = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
@@ -55,11 +105,10 @@ export function currentDateTime(props) {
   }).format(date);
 
   console.log(formattedDate);
-  
-  
+
   // Calculate the time difference in hours
   let hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60));
-  
+
   // Generate the output based on the time difference
   let output;
   if (hoursDifference < 24) {
@@ -76,24 +125,27 @@ export const renderParagraphWithLinks = (apiResponse) => {
     return <p>Loading...</p>;
   }
   const urlRegex = /(https?|ftp):\/\/[^\s/$.?#].[^\s]*/gi;
-  
+
   // Replace links with clickable anchors in the paragraph
   const paragraphWithLinks = apiResponse.replace(
-    new RegExp(urlRegex.source, 'gi'),
+    new RegExp(urlRegex.source, "gi"),
     (match) => `<a href="${match}" target="_blank" rel="desc_link">${match}</a>`
   );
-  
-  return <p className="leading-loose text-[0.955rem] text-black  description" dangerouslySetInnerHTML={{ __html: paragraphWithLinks }} />
+
+  return (
+    <p
+      className="leading-loose text-[0.955rem] text-black  description"
+      dangerouslySetInnerHTML={{ __html: paragraphWithLinks }}
+    />
+  );
 };
 
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
 
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
-
-export const LIVE_CHAT_COUNT=10;
+export const LIVE_CHAT_COUNT = 10;
 
 export const GOOGLE_API_KEY = process.env.REACT_APP_VIBETUBE;
 
-export const YOUTUBE_VIDEOS_API2=
-"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=16"
-
-
+export const YOUTUBE_VIDEOS_API2 =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=16";
